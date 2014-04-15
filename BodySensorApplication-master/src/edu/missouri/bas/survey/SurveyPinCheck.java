43a550985422eb7f2f4dcf6eaba9698865672121
Ricky <rs79c@mail.missouri.edu>
2014-04-14 20:25:41 -0500

    Morning Report Part1

--- BodySensorApplication-master/src/edu/missouri/bas/survey/SurveyPinCheck.java
+++ BodySensorApplication-master/src/edu/missouri/bas/survey/SurveyPinCheck.java
@@ -1,8 +1,20 @@
 package edu.missouri.bas.survey;
 
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -10,7 +22,11 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.MediaPlayer;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Vibrator;
@@ -20,7 +36,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 import edu.missouri.bas.R;
+import edu.missouri.bas.activities.AdminManageActivity;
 import edu.missouri.bas.service.SensorService;
 
 
@@ -38,8 +56,8 @@ public class SurveyPinCheck extends Activity {
 	private AlertDialog diaLog;
 	//Ricky 4/1/14
 	private int randomSeq;
-	
-    
+	public static Context SurveyPinContext;
+	String errMSG ="Please check your wifi or dataplan.\r\nThe phone is offline now.";
 
     private class StartSound extends TimerTask {
     	@Override    	
@@ -71,7 +89,9 @@ public class SurveyPinCheck extends Activity {
 		//call pin-Dialog function
 		createPinAlertDialog();	   
 		
-		
+		SurveyPinContext = this;
+		SharedPreferences shp = getSharedPreferences("PINLOGIN", Context.MODE_PRIVATE);
+	    String ID = shp.getString(AdminManageActivity.ASID, "");
 		
 	    bButton = (Button)findViewById(R.id.button_exit);
 	    pButton = (Button)findViewById(R.id.button_pin);
@@ -118,6 +138,9 @@ public class SurveyPinCheck extends Activity {
 			Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
 	        v.vibrate(1000);
 	        surveyName = "MORNING_REPORT";
+	        //Automatically triggered Morning Survey
+	        TransmitData transmitData=new TransmitData();
+			transmitData.execute("Trigger."+ID+"."+Calendar.getInstance().get(Calendar//TODO.),Calendar.getInstance().getTime().toString()+"Morning Survey is automatically started\n");
 		}
 		//ADD VOICE AND VIBRATE CONTROL TO THE DRINKFOLLOWUP
 		if(surveyName.equalsIgnoreCase("DRINKING_FOLLOWUP") && surveyFile.equalsIgnoreCase("DrinkingFollowup.xml"))
@@ -266,4 +289,65 @@ public class SurveyPinCheck extends Activity {
     	//cancelAllTimerTask();
     	super.onDestroy();
     }
+	
+	private class TransmitData extends AsyncTask<String,Void, Boolean>
+	{
+
+		@Override
+		protected Boolean doInBackground(String... strings) {
+			// TODO Auto-generated method stub
+			 String fileName=strings[0];
+	         String dataToSend=strings[1];
+	         if(checkDataConnectivity())
+	 		{
+	         HttpPost request = new HttpPost("http://dslsrv8.cs.missouri.edu/~rs79c/Server/Crt/writeArrayToFile.php");
+	         //HttpPost request = new HttpPost("http://dslsrv8.cs.missouri.edu/~rs79c/Server/Test/writeArrayToFile.php");
+	         List<NameValuePair> params = new ArrayList<NameValuePair>();
+	         //file_name 
+	         params.add(new BasicNameValuePair("file_name",fileName));        
+	         //data                       
+	         params.add(new BasicNameValuePair("data",dataToSend));
+	         try {
+	         	        	
+	             request.setEntity(new UrlEncodedFormEntity(params, HTTP.UTF_8));
+	             HttpResponse response = new DefaultHttpClient().execute(request);
+	             if(response.getStatusLine().getStatusCode() == 200){
+	                 String result = EntityUtils.toString(response.getEntity());
+	                 Log.d("Sensor Data Point Info",result);                
+	                // Log.d("Wrist Sensor Data Point Info","Data Point Successfully Uploaded!");
+	             }
+	             return true;
+	         } 
+	         catch (Exception e) 
+	         {	             
+	             e.printStackTrace();
+	             return false;
+	         }
+	 	  }
+	     	
+	     else 
+	     {
+	     	Log.d("Sensor Data Point Info","No Network Connection:Data Point was not uploaded");
+	     	Toast.makeText(SurveyPinContext, errMSG, Toast.LENGTH_LONG).show();
+	     	return false;
+	      } 
+		    
+		}
+		
+	}
+	 public static boolean checkDataConnectivity() {
+	    	ConnectivityManager connectivity = (ConnectivityManager) SurveyPinContext
+					.getSystemService(Context.CONNECTIVITY_SERVICE);
+			if (connectivity != null) {
+				NetworkInfo[] info = connectivity.getAllNetworkInfo();
+				if (info != null) {
+					for (int i = 0; i < info.length; i++) {
+						if (info[i].getState() == NetworkInfo.State.CONNECTED) {
+							return true;
+						}
+					}
+				}
+			}
+			return false;
+		}
 }
